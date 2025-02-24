3e3c749d4563b357c3cd4830dea9731a5c962ee3
Qwinci <qwinci222@gmail.com>
2025-02-24 18:00:55 +0200

    Support 32-bit member pointers in container_of

--- include/hz/container_of.hpp
+++ include/hz/container_of.hpp
@@ -9,11 +9,16 @@
 namespace hz {
 	template<typename T, typename M>
 	T* container_of(M* ptr, M T::* m) {
-		static_assert(sizeof(m) == sizeof(uintptr_t));
-
-		uintptr_t offset;
-		__builtin_memcpy(&offset, &m, sizeof(uintptr_t));
-
-		return std::launder(reinterpret_cast<T*>(reinterpret_cast<char*>(ptr) - offset));
+		if constexpr (sizeof(m) == sizeof(uintptr_t)) {
+			uintptr_t offset = 0;
+			__builtin_memcpy(&offset, &m, sizeof(uintptr_t));
+			return std::launder(reinterpret_cast<T*>(reinterpret_cast<char*>(ptr) - offset));
+		}
+		else {
+			static_assert(sizeof(m) == 4);
+			uint32_t offset = 0;
+			__builtin_memcpy(&offset, &m, 4);
+			return std::launder(reinterpret_cast<T*>(reinterpret_cast<char*>(ptr) - offset));
+		}
 	}
 }
